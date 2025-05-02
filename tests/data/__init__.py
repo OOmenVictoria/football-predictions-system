a89df4113e18db2fd8a54ba07a1a84db69c4958b
OOmenVictoria <omenvictoria@gmail.com>
2025-05-02 17:46:22 +0200

    Create __init__.py

---
+++ tests/data/__init__.py
@@ -0,0 +1,5 @@
+"""
+Package di test per i moduli di raccolta dati.
+Questo package contiene test per i moduli che si occupano del recupero
+e dell'elaborazione dei dati calcistici da varie fonti.
+"""
