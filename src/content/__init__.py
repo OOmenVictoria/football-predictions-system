d603c9b80236e7b5ca82739b7ff80bbf4aadd402
OOmenVictoria <omenvictoria@gmail.com>
2025-05-02 17:34:21 +0200

    Create __init__.py

---
+++ src/content/__init__.py
@@ -0,0 +1,7 @@
+"""
+Pacchetto per la generazione di contenuti e articoli.
+Questo pacchetto fornisce moduli per generare articoli di pronostico calcistico
+utilizzando i dati raccolti e le analisi generate.
+"""
+
+from src.content.generator import generate_article, generate_articles_batch
