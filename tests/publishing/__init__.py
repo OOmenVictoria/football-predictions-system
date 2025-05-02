e5d572eb5250532c087e8494c10ab3a98888039a
OOmenVictoria <omenvictoria@gmail.com>
2025-05-02 17:47:04 +0200

    Create __init__.py

---
+++ tests/publishing/__init__.py
@@ -0,0 +1,5 @@
+"""
+Package di test per i moduli di pubblicazione.
+Questo package contiene test per verificare la corretta funzionalità
+dei moduli che gestiscono la pubblicazione di articoli su WordPress.
+"""
