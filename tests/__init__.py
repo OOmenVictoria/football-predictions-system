773352360cf499f9b8b6831c9d8429c82e672baf
OOmenVictoria <omenvictoria@gmail.com>
2025-05-02 17:46:03 +0200

    Create __init__.py

---
+++ tests/__init__.py
@@ -0,0 +1,5 @@
+"""
+Package di test per il sistema di pronostici calcistici.
+Questo package contiene test unitari e di integrazione per verificare
+il corretto funzionamento di tutti i componenti del sistema.
+"""
